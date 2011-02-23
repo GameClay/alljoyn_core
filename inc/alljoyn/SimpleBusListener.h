629a48bdfec3d7b2d7dc474053b08696754da182
Greg Burns <gburns@quicinc.com>
2011-02-23 15:15:14 -0800

    Helper class for synchronous handling of bus events.

---
+++ inc/alljoyn/SimpleBusListener.h
@@ -0,0 +1,179 @@
+/**
+ * @file
+ * SimpleBusListener is a syncronous bus listener that fits the need of applications that can
+ * handled all bus events from the main thread.
+ */
+
+/******************************************************************************
+ * Copyright 2009-2011, Qualcomm Innovation Center, Inc.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ ******************************************************************************/
+#ifndef _ALLJOYN_SIMPLEBUSLISTENER_H
+#define _ALLJOYN_SIMPLEBUSLISTENER_H
+
+#ifndef __cplusplus
+#error Only include SimpleBusListener.h in C++ code.
+#endif
+
+#include <alljoyn/Session.h>
+#include <alljoyn/QosInfo.h>
+#include <alljoyn/BusListener.h>
+
+namespace ajn {
+
+/**
+ * Helper class that provides a blocking API that allows application threads to wait for bus events.
+ */
+class SimpleBusListener : public BusListener {
+  public:
+
+    /**
+     * The various bus events.
+     */
+    static const uint32_t FOUND_ADVERTISED_NAME = 0x0001;
+    static const uint32_t LOST_ADVERTISED_NAME  = 0x0002;
+    static const uint32_t NAME_OWNER_CHANGED    = 0x0004;
+    static const uint32_t SESSION_LOST          = 0x0008;
+    static const uint32_t ACCEPT_SESSION        = 0x0010;
+    static const uint32_t ALL_EVENTS            = 0x00FF;
+    static const uint32_t NO_EVENT              = 0x0000;
+
+    static const uint32_t FOREVER = -1;
+
+    /**
+     * Constructor that intializes a bus listener with specific events enabled.
+     *
+     * @param enabled   A logical OR of the bus events to be enabled for this listener.
+     */
+    SimpleBusListener(uint32_t enabled = 0);
+
+    /**
+     * Set an event filter. This overrides the events enabled by the constructor. Any queued events
+     * that are not enabled are discarded.
+     *
+     * @param enabled  A logical OR of the bus events to be enabled for this listener.
+     */
+    void SetFilter(uint32_t enabled);
+
+    /**
+     * This union is used to return busEvent information for the bus events.
+     */
+    class BusEvent {
+        friend class SimpleBusListener;
+      public:
+        uint32_t eventType;             ///< The busEvent type identifies which variant from the union applies.
+        union {
+            struct {
+                const char* name;       ///< well known name that the remote bus is advertising that is of interest to this attachment.
+                const QosInfo* advQos;  ///< Advertised quality of service.
+                const char* namePrefix; ///< The well-known name prefix used in call to FindAdvertisedName that triggered the busEvent.
+            } foundAdvertisedName;
+            struct {
+                const char* name;       ///< A well known name that the remote bus is advertising that is of interest to this attachment.
+                const char* namePrefix; ///< The well-known name prefix that was used in a call to FindAdvertisedName that triggered this callback.
+            } lostAdvertisedName;
+            struct {
+                const char* busName;       ///< The well-known name that has changed.
+                const char* previousOwner; ///< The unique name that previously owned the name or NULL if there was no previous owner.
+                const char* newOwner;      ///< The unique name that now owns the name or NULL if the there is no new owner.
+            } nameOwnerChanged;
+            struct {
+                SessionId sessionId;     ///< Id of session that was lost.
+            } sessionLost;
+            struct {
+                const char* sessionName; ///< Name of session.
+                SessionId id;            ///< Id of session.
+                const char* joiner;      ///< Unique name of potential joiner.
+                const QosInfo* qos;      ///< Incoming quality of service.
+            } acceptSession;
+        };
+
+        /**
+         * Constructor
+         */
+        BusEvent() : eventType(0) {}
+
+        /**
+         * Copy constructor.
+         */
+        BusEvent(const BusEvent& other) { *this = other; }
+
+        /**
+         * Assignment operator.
+         */
+        BusEvent& operator=(const BusEvent& other);
+
+      private:
+
+        /**
+         * @internal  Storage for the busEvent strings.
+         */
+        qcc::String strings;
+        /**
+         * @internal  Storage for quality of service information.
+         */
+        QosInfo qosInfo;
+    };
+
+    /**
+     * Wait for a bus event.
+     *
+     * @param busEvent Returns the busEvent type and related information.
+     * @param timeout  A timeout in milliseconds to wait for the busEvent, 0 means don't wait just
+     *                 check for an busEvent and return, FOREVER (-1) means wait forever.
+     *
+     * @return #ER_OK if an even was received.
+     *         #ER_TIMEOUT if the wait timed out.
+     *         #ER_THREAD_ALERTED if the wait unblocked due to a signal
+     */
+    QStatus WaitForEvent(BusEvent& busEvent, uint32_t timeout = FOREVER);
+
+    /**
+     * On receiving an ACCEPT_SESSION busEvent the application must call this function to accept or
+     * rejct the session request. Calling WaitForEvent will automatically reject the session request.
+     *
+     * @param accept  Select or reject this session request.
+     */
+    QStatus AcceptSession(bool accept);
+
+    /**
+     * Desctructor.
+     */
+    ~SimpleBusListener();
+
+  private:
+
+    /**
+     * Bit mask of events enabled for this listener.
+     */
+    uint32_t enabled;
+
+    void FoundAdvertisedName(const char* name, const QosInfo& advQos, const char* namePrefix);
+    void LostAdvertisedName(const char* name, const char* namePrefix);
+    void NameOwnerChanged(const char* busName, const char* previousOwner, const char* newOwner);
+    void SessionLost(const SessionId& sessionId);
+    bool AcceptSession(const char* sessionName, SessionId id, const char* joiner, const QosInfo& qos);
+
+    /**
+     * @internal
+     * Internal storage for this class.
+     */
+    class Internal;
+    Internal& internal;
+
+};
+
+}
+
+#endif
---
+++ src/SimpleBusListener.cc
@@ -0,0 +1,279 @@
+/**
+ * @file
+ *
+ * This file implements the default bus listener.
+ */
+
+/******************************************************************************
+ * Copyright 2009-2011, Qualcomm Innovation Center, Inc.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ ******************************************************************************/
+
+#include <qcc/platform.h>
+
+#include <queue>
+
+#include <qcc/String.h>
+#include <qcc/Event.h>
+#include <qcc/Mutex.h>
+#include <qcc/Thread.h>
+#include <qcc/Debug.h>
+
+#include <alljoyn/BusListener.h>
+#include <alljoyn/SimpleBusListener.h>
+
+
+#define QCC_MODULE "ALLJOYN"
+
+
+using namespace qcc;
+using namespace std;
+
+namespace ajn {
+
+SimpleBusListener::BusEvent& SimpleBusListener::BusEvent::operator=(const BusEvent& other)
+{
+    eventType = other.eventType;
+    strings = other.strings;
+    qosInfo = other.qosInfo;
+    const char* pos = strings.c_str();
+    switch (eventType) {
+    case FOUND_ADVERTISED_NAME:
+        foundAdvertisedName.name = pos;
+        foundAdvertisedName.advQos = &qosInfo;
+        pos += strlen(foundAdvertisedName.name) + 1;
+        foundAdvertisedName.namePrefix = pos;
+        break;
+    case LOST_ADVERTISED_NAME:
+        lostAdvertisedName.name = pos;
+        pos += strlen(other.lostAdvertisedName.name) + 1;
+        lostAdvertisedName.namePrefix = pos;
+        break;
+    case NAME_OWNER_CHANGED:
+        nameOwnerChanged.busName = pos;
+        pos += strlen(nameOwnerChanged.busName) + 1;
+        if (other.nameOwnerChanged.previousOwner) {
+            nameOwnerChanged.previousOwner = pos;
+            pos += strlen(nameOwnerChanged.previousOwner) + 1;
+        } else {
+            nameOwnerChanged.previousOwner = NULL;
+        }
+        if (other.nameOwnerChanged.newOwner) {
+            nameOwnerChanged.newOwner = pos;
+        } else {
+            nameOwnerChanged.newOwner = NULL;
+        }
+        break;
+    case SESSION_LOST:
+        sessionLost.sessionId = other.sessionLost.sessionId;
+        break;
+    case ACCEPT_SESSION:
+        acceptSession.sessionName = pos;
+        acceptSession.id = other.acceptSession.id;
+        pos += strlen(acceptSession.sessionName) + 1;
+        acceptSession.joiner = pos;
+        acceptSession.qos = &qosInfo;
+        break;
+    default:
+        break;
+    }
+    return *this;
+};
+
+class SimpleBusListener::Internal
+{
+  public:
+    Internal() : acceptEvent(NULL), accepted(false), numWaiters(0) {}
+    Event waitEvent;
+    qcc::Mutex lock;
+    std::queue<BusEvent> eventQueue;
+    Event* acceptEvent;
+    bool accepted;
+    uint32_t numWaiters;
+
+    void QueueEvent(BusEvent& ev) {
+        lock.Lock();
+        eventQueue.push(ev);
+        waitEvent.SetEvent();
+        lock.Unlock();
+    }
+};
+
+SimpleBusListener::SimpleBusListener(uint32_t enabled) : enabled(enabled), internal(*(new Internal)) {}
+
+void SimpleBusListener::FoundAdvertisedName(const char* name, const QosInfo& advQos, const char* namePrefix)
+{
+    if (enabled & FOUND_ADVERTISED_NAME) {
+        BusEvent busEvent;
+        busEvent.eventType = FOUND_ADVERTISED_NAME;
+        busEvent.foundAdvertisedName.name = name;
+        busEvent.foundAdvertisedName.advQos = &advQos;
+        busEvent.foundAdvertisedName.namePrefix = namePrefix;
+        internal.QueueEvent(busEvent);
+    }
+}
+
+void SimpleBusListener::LostAdvertisedName(const char* name, const char* namePrefix)
+{
+    if (enabled & LOST_ADVERTISED_NAME) {
+        BusEvent busEvent;
+        busEvent.eventType = LOST_ADVERTISED_NAME;
+        busEvent.lostAdvertisedName.name = name;
+        busEvent.lostAdvertisedName.namePrefix = namePrefix;
+        internal.QueueEvent(busEvent);
+    }
+}
+
+void SimpleBusListener::NameOwnerChanged(const char* busName, const char* previousOwner, const char* newOwner)
+{
+    if (enabled & NAME_OWNER_CHANGED) {
+        BusEvent busEvent;
+        busEvent.eventType = NAME_OWNER_CHANGED;
+        busEvent.nameOwnerChanged.busName = busName;
+        busEvent.nameOwnerChanged.previousOwner = previousOwner;
+        busEvent.nameOwnerChanged.newOwner = newOwner;
+        internal.QueueEvent(busEvent);
+    }
+}
+
+void SimpleBusListener::SessionLost(const SessionId& sessionId)
+{
+    if (enabled & SESSION_LOST) {
+        BusEvent busEvent;
+        busEvent.eventType = SESSION_LOST;
+        busEvent.sessionLost.sessionId = sessionId;
+        internal.QueueEvent(busEvent);
+    }
+}
+
+bool SimpleBusListener::AcceptSession(const char* sessionName, SessionId id, const char* joiner, const QosInfo& qos)
+{
+    if (enabled & ACCEPT_SESSION) {
+        BusEvent busEvent;
+        busEvent.eventType = ACCEPT_SESSION;
+        busEvent.acceptSession.sessionName = sessionName;
+        busEvent.acceptSession.id = id;
+        busEvent.acceptSession.joiner = joiner;
+        busEvent.acceptSession.qos = &qos;
+        internal.QueueEvent(busEvent);
+        internal.lock.Lock();
+        if (!internal.acceptEvent) {
+            Event acceptEvent;
+            internal.acceptEvent = &acceptEvent;
+            internal.lock.Unlock();
+            Event::Wait(acceptEvent);
+            return internal.accepted;
+        } else {
+            internal.acceptEvent->SetEvent();
+            internal.acceptEvent = NULL;
+            internal.lock.Unlock();
+        }
+    }
+    return false;
+}
+
+QStatus SimpleBusListener::AcceptSession(bool accept)
+{
+    QStatus status = ER_OK;
+    internal.lock.Lock();
+    if (internal.acceptEvent) {
+        internal.accepted = accept;
+        internal.acceptEvent->SetEvent();
+        internal.acceptEvent = NULL;
+    } else {
+        status = ER_BUS_NO_SESSION;
+    }
+    internal.lock.Unlock();
+    return status;
+}
+
+
+void SimpleBusListener::SetFilter(uint32_t enabled)
+{
+    internal.lock.Lock();
+    this->enabled = enabled;
+    if (internal.acceptEvent) {
+        AcceptSession(false);
+    }
+    /*
+     * Save all queued events that pass the filter.
+     */
+    size_t pass = 0;
+    while (internal.eventQueue.size() != pass) {
+        BusEvent ev = internal.eventQueue.front();
+        internal.eventQueue.pop();
+        if (ev.eventType & enabled) {
+            internal.eventQueue.push(ev);
+            ++pass;
+        }
+    }
+    if (pass == 0) {
+        internal.waitEvent.ResetEvent();
+    }
+    internal.lock.Unlock();
+}
+
+QStatus SimpleBusListener::WaitForEvent(BusEvent& busEvent, uint32_t timeout) 
+{
+    QStatus status = ER_OK;
+    internal.lock.Lock();
+    busEvent.eventType = NO_EVENT;
+    if (internal.numWaiters > 0) {
+        status = ER_BUS_WAIT_FAILED;
+        QCC_LogError(status, ("Another thread is already waiting"));
+        goto ExitWait;
+    }
+    if (internal.acceptEvent) {
+        status = ER_BUS_WAIT_FAILED;
+        QCC_LogError(status, ("An ACCEPT_SESSION event has not been accepted"));
+        goto ExitWait;
+    }
+    if (internal.eventQueue.empty() && timeout) {
+        ++internal.numWaiters;
+        internal.lock.Unlock();
+        status = Event::Wait(internal.waitEvent, (timeout == -1) ? Event::WAIT_FOREVER : timeout);
+        internal.lock.Lock();
+        internal.waitEvent.ResetEvent();
+        --internal.numWaiters;
+    }
+    if (!internal.eventQueue.empty()) {
+        busEvent = internal.eventQueue.front();
+        internal.eventQueue.pop();
+    }
+
+ExitWait:
+    internal.lock.Unlock();
+    return status;
+}
+
+SimpleBusListener::~SimpleBusListener()
+{
+    /*
+     * Check there are no threads waiting
+     */
+    internal.lock.Lock();
+    while (internal.numWaiters > 0) {
+        internal.waitEvent.SetEvent();
+        internal.lock.Unlock();
+        qcc::Sleep(5);
+        internal.lock.Lock();
+    }
+    if (internal.acceptEvent) {
+        internal.acceptEvent->SetEvent();
+    }
+    internal.lock.Unlock();
+    delete &internal;
+}
+
+}
