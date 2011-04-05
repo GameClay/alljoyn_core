56ad175be015aee2bca86cc8f66c34481b494b54
George Nash <georgen@quicinc.com>
2011-04-05 13:06:37 -0700

    whitespace fix

--- src/ProxyBusObject.cc
+++ src/ProxyBusObject.cc
@@ -117,7 +117,7 @@ QStatus ProxyBusObject::GetProperty(const char* iface, const char* property, Msg
         size_t numArgs = ArraySize(inArgs);
         MsgArg::Set(inArgs, numArgs, "ss", iface, property);
         const InterfaceDescription* propIface = bus->GetInterface(org::freedesktop::DBus::Properties::InterfaceName);
-        if ( propIface == NULL) {
+        if (propIface == NULL) {
             status = ER_BUS_NO_SUCH_INTERFACE;
         } else {
             status = MethodCall(*(propIface->GetMember("Get")),
