3f5022da305a01e7fa5b1bd57923ab20be607611
Steve Kinneberg <stevek@quicinc.com>
2011-04-25 16:48:28 -0700

    Fix build warning.

--- daemon/AllJoynObj.cc
+++ daemon/AllJoynObj.cc
@@ -2022,7 +2022,6 @@ void AllJoynObj::ExchangeNamesSignalHandler(const InterfaceDescription::Member* 
         set<String> sendSet;
         router.LockNameTable();
         b2bEndpointsLock.Lock();
-        bool isRemarshaled = false;
         map<qcc::StringMapKey, RemoteEndpoint*>::const_iterator bit = b2bEndpoints.find(msg->GetRcvEndpointName());
         map<qcc::StringMapKey, RemoteEndpoint*>::iterator it = b2bEndpoints.begin();
         while (it != b2bEndpoints.end()) {
