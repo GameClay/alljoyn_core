f2bfbbd2c5ca193a3c7117a8ecd8b0f93ae0fb59
Greg Burns <gburns@quicinc.com>
2011-07-07 12:41:25 -0700

    Wrong starting address for message header.

--- src/AllJoynCrypto.cc
+++ src/AllJoynCrypto.cc
@@ -89,7 +89,7 @@ QStatus Crypto::Encrypt(const Message& message, const KeyBlob& keyBlob, uint8_t*
     case KeyBlob::AES:
     {
         uint8_t* body = msgBuf + hdrLen;
-        uint8_t nd[Crypto_AES::CCM_NONCE_SIZE];
+        uint8_t nd[5];
         uint32_t serial = message->GetCallSerial();
 
         nd[0] = (uint8_t)keyBlob.GetRole();
@@ -97,7 +97,6 @@ QStatus Crypto::Encrypt(const Message& message, const KeyBlob& keyBlob, uint8_t*
         nd[2] = (uint8_t)(serial >> 16);
         nd[3] = (uint8_t)(serial >> 8);
         nd[4] = (uint8_t)(serial);
-        memset(nd + 5, 0, 6);
         KeyBlob nonce(nd, sizeof(nd), KeyBlob::GENERIC);
 
         QCC_DbgHLPrintf(("Encrypt key:   %s", BytesToHexString(keyBlob.GetData(), keyBlob.GetSize()).c_str()));
@@ -109,7 +108,7 @@ QStatus Crypto::Encrypt(const Message& message, const KeyBlob& keyBlob, uint8_t*
              * To prevent an attack where the attacker sends a bogus expansion rule we
              * authenticate the compressed headers even though we won't be sending them.
              */
-            qcc::String extHdr = ConcatenateCompressedFields(msgBuf + hdrLen, hdrLen, message->GetHeaderFields());
+            qcc::String extHdr = ConcatenateCompressedFields(msgBuf, hdrLen, message->GetHeaderFields());
             status = aes.Encrypt_CCM(body, body, bodyLen, nonce, extHdr.data(), extHdr.size());
         } else {
             status = aes.Encrypt_CCM(body, body, bodyLen, nonce, msgBuf, hdrLen);
@@ -132,7 +131,7 @@ QStatus Crypto::Decrypt(const Message& message, const KeyBlob& keyBlob, uint8_t*
     case KeyBlob::AES:
     {
         uint8_t* body = msgBuf + hdrLen;
-        uint8_t nd[Crypto_AES::CCM_NONCE_SIZE];
+        uint8_t nd[5];
         uint32_t serial = message->GetCallSerial();
 
         nd[0] = (uint8_t)keyBlob.GetAntiRole();
@@ -140,7 +139,6 @@ QStatus Crypto::Decrypt(const Message& message, const KeyBlob& keyBlob, uint8_t*
         nd[2] = (uint8_t)(serial >> 16);
         nd[3] = (uint8_t)(serial >> 8);
         nd[4] = (uint8_t)(serial);
-        memset(nd + 5, 0, 6);
         KeyBlob nonce(nd, sizeof(nd), KeyBlob::GENERIC);
 
         QCC_DbgHLPrintf(("Decrypt key:   %s", BytesToHexString(keyBlob.GetData(), keyBlob.GetSize()).c_str()));
@@ -152,7 +150,7 @@ QStatus Crypto::Decrypt(const Message& message, const KeyBlob& keyBlob, uint8_t*
              * To prevent an attack where the attacker sends a bogus expansion rule we
              * authenticate the compressed headers even though we won't be sending them.
              */
-            qcc::String extHdr = ConcatenateCompressedFields(msgBuf + hdrLen, hdrLen, message->GetHeaderFields());
+            qcc::String extHdr = ConcatenateCompressedFields(msgBuf, hdrLen, message->GetHeaderFields());
             status = aes.Decrypt_CCM(body, body, bodyLen, nonce, extHdr.data(), extHdr.size());
         } else {
             status = aes.Decrypt_CCM(body, body, bodyLen, nonce, msgBuf, hdrLen);
