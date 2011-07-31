91077bacb55511e9038090f52ef83043ba412a68
Greg Burns <gburns@quicinc.com>
2011-07-31 08:53:48 -0700

    Added stress testing.

--- test/srp.cc
+++ test/srp.cc
@@ -59,108 +59,117 @@ int main(int argc, char** argv)
         status = srp.TestVector();
         if (status != ER_OK) {
             printf("Test vector failed\n");
+            exit(1);
         }
     }
-    printf("#################################\n");
+    printf("############Test vector passed ################\n");
     /*
      * Basic API test.
      */
-    {
-        Crypto_SRP client;
-        Crypto_SRP server;
+    for (int i = 0; i < 100; ++i) {
+        {
+            Crypto_SRP client;
+            Crypto_SRP server;
 
-        status = server.ServerInit(user, pwd, toClient);
-        if (status != ER_OK) {
-            QCC_LogError(status, ("SRP ServerInit failed"));
-            goto TestFail;
+            status = server.ServerInit(user, pwd, toClient);
+            if (status != ER_OK) {
+                QCC_LogError(status, ("SRP ServerInit failed"));
+                goto TestFail;
+            }
+            status = client.ClientInit(toClient, toServer);
+            if (status != ER_OK) {
+                QCC_LogError(status, ("SRP ClientInit failed"));
+                goto TestFail;
+            }
+            status = server.ServerFinish(toServer);
+            if (status != ER_OK) {
+                QCC_LogError(status, ("SRP ServerFinish failed"));
+                goto TestFail;
+            }
+            status = client.ClientFinish(user, pwd);
+            if (status != ER_OK) {
+                QCC_LogError(status, ("SRP ClientFinish failed"));
+                goto TestFail;
+            }
+            /*
+             * Check premaster secrets match
+             */
+            server.GetPremasterSecret(serverPMS);
+            client.GetPremasterSecret(clientPMS);
+            if (clientPMS.GetSize() != serverPMS.GetSize()) {
+                printf("Premaster secrets have different sizes\n");
+                goto TestFail;
+            }
+            if (memcmp(serverPMS.GetData(), clientPMS.GetData(), serverPMS.GetSize()) != 0) {
+                printf("Premaster secrets don't match\n");
+                printf("client = %s\n", BytesToHexString(serverPMS.GetData(), serverPMS.GetSize()).c_str());
+                printf("server = %s\n", BytesToHexString(clientPMS.GetData(), clientPMS.GetSize()).c_str());
+                goto TestFail;
+            }
+            printf("Premaster secret = %s\n", BytesToHexString(serverPMS.GetData(), serverPMS.GetSize()).c_str());
+            verifier = server.ServerGetVerifier();
         }
-        status = client.ClientInit(toClient, toServer);
-        if (status != ER_OK) {
-            QCC_LogError(status, ("SRP ClientInit failed"));
-            goto TestFail;
-        }
-        status = server.ServerFinish(toServer);
-        if (status != ER_OK) {
-            QCC_LogError(status, ("SRP ServerFinish failed"));
-            goto TestFail;
-        }
-        status = client.ClientFinish(user, pwd);
-        if (status != ER_OK) {
-            QCC_LogError(status, ("SRP ClientFinish failed"));
-            goto TestFail;
-        }
+        printf("###### Checking verifier ########\n");
         /*
-         * Check premaster secrets match
+         * Test using the verifier from the previous test.
          */
-        server.GetPremasterSecret(serverPMS);
-        client.GetPremasterSecret(clientPMS);
-        if (clientPMS.GetSize() != serverPMS.GetSize()) {
-            printf("Premaster secrets have different sizes\n");
-            goto TestFail;
+        {
+            Crypto_SRP client;
+            Crypto_SRP server;
+
+            status = server.ServerInit(verifier, toClient);
+            if (status != ER_OK) {
+                QCC_LogError(status, ("SRP ServerInit failed"));
+                goto TestFail;
+            }
+            status = client.ClientInit(toClient, toServer);
+            if (status != ER_OK) {
+                QCC_LogError(status, ("SRP ClientInit failed"));
+                goto TestFail;
+            }
+            status = server.ServerFinish(toServer);
+            if (status != ER_OK) {
+                QCC_LogError(status, ("SRP ServerFinish failed"));
+                goto TestFail;
+            }
+            status = client.ClientFinish(user, pwd);
+            if (status != ER_OK) {
+                QCC_LogError(status, ("SRP ClientFinish failed"));
+                goto TestFail;
+            }
+            /*
+             * Check premaster secrets match
+             */
+            server.GetPremasterSecret(serverPMS);
+            client.GetPremasterSecret(clientPMS);
+            if (clientPMS.GetSize() != serverPMS.GetSize()) {
+                printf("Premaster secrets have different sizes\n");
+                goto TestFail;
+            }
+            if (memcmp(serverPMS.GetData(), clientPMS.GetData(), serverPMS.GetSize()) != 0) {
+                printf("Premaster secrets don't match\n");
+                printf("client = %s\n", BytesToHexString(serverPMS.GetData(), serverPMS.GetSize()).c_str());
+                printf("server = %s\n", BytesToHexString(clientPMS.GetData(), clientPMS.GetSize()).c_str());
+                goto TestFail;
+            }
+            printf("Premaster secret = %s\n", BytesToHexString(serverPMS.GetData(), serverPMS.GetSize()).c_str());
+
+            qcc::String serverRand = RandHexString(64);
+            qcc::String clientRand = RandHexString(64);
+            uint8_t masterSecret[48];
+
+            printf("testing pseudo random function\n");
+
+            status = Crypto_PseudorandomFunction(serverPMS, "foobar", serverRand + clientRand, masterSecret, sizeof(masterSecret));
+            if (status != ER_OK) {
+                QCC_LogError(status, ("Crypto_PseudoRandomFunction failed"));
+                goto TestFail;
+            }
+            printf("Master secret = %s\n", BytesToHexString(masterSecret, sizeof(masterSecret)).c_str());
+
         }
-        if (memcmp(serverPMS.GetData(), clientPMS.GetData(), serverPMS.GetSize()) != 0) {
-            printf("Premaster secrets don't match\n");
-            goto TestFail;
-        }
-        printf("Premaster secret = %s\n", BytesToHexString(serverPMS.GetData(), serverPMS.GetSize()).c_str());
-        verifier = server.ServerGetVerifier();
+        printf("#################################\n");
     }
-    printf("#################################\n");
-    /*
-     * Test using the verifier from the previous test.
-     */
-    {
-        Crypto_SRP client;
-        Crypto_SRP server;
-
-        status = server.ServerInit(verifier, toClient);
-        if (status != ER_OK) {
-            QCC_LogError(status, ("SRP ServerInit failed"));
-            goto TestFail;
-        }
-        status = client.ClientInit(toClient, toServer);
-        if (status != ER_OK) {
-            QCC_LogError(status, ("SRP ClientInit failed"));
-            goto TestFail;
-        }
-        status = server.ServerFinish(toServer);
-        if (status != ER_OK) {
-            QCC_LogError(status, ("SRP ServerFinish failed"));
-            goto TestFail;
-        }
-        status = client.ClientFinish(user, pwd);
-        if (status != ER_OK) {
-            QCC_LogError(status, ("SRP ClientFinish failed"));
-            goto TestFail;
-        }
-        /*
-         * Check premaster secrets match
-         */
-        server.GetPremasterSecret(serverPMS);
-        client.GetPremasterSecret(clientPMS);
-        if (clientPMS.GetSize() != serverPMS.GetSize()) {
-            printf("Premaster secrets have different sizes\n");
-            goto TestFail;
-        }
-        if (memcmp(serverPMS.GetData(), clientPMS.GetData(), serverPMS.GetSize()) != 0) {
-            printf("Premaster secrets don't match\n");
-            goto TestFail;
-        }
-        printf("Premaster secret = %s\n", BytesToHexString(serverPMS.GetData(), serverPMS.GetSize()).c_str());
-
-        qcc::String serverRand = RandHexString(64);
-        qcc::String clientRand = RandHexString(64);
-        uint8_t masterSecret[48];
-
-        status = Crypto_PseudorandomFunction(serverPMS, "foobar", serverRand + clientRand, masterSecret, sizeof(masterSecret));
-        if (status != ER_OK) {
-            QCC_LogError(status, ("Crypto_PseudoRandomFunction failed"));
-            goto TestFail;
-        }
-        printf("Master secret = %s\n", BytesToHexString(masterSecret, sizeof(masterSecret)).c_str());
-
-    }
-    printf("#################################\n");
 
     printf("Passed\n");
     return 0;
