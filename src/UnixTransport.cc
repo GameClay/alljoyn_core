GameClay/alljoyn_core

commit 63c988325161a3d9d37d3054defffa294dd227ef

Daemon port to darwin.
Functional. Passes WS Checks

src/UnixTransport.cc
@@ -30,6 +30,11 @@
 #include <qcc/StringUtil.h>
 #include <qcc/Util.h>
 
+#include <sys/un.h>
+#if defined(QCC_OS_DARWIN)
+#include <sys/ucred.h>
+#endif
+
 #include <alljoyn/BusAttachment.h>
 
 #include "BusInternal.h"
@@ -236,6 +241,69 @@ QStatus UnixTransport::NormalizeTransportSpec(const char* inSpec, qcc::String& o
     return status;
 }
 
+static QStatus SendSocketCreds(SocketFd sockFd, uid_t uid, gid_t gid, pid_t pid)
+{
+#if defined(QCC_OS_DARWIN)
+    // Socket credentials on Darwin are exchanged using getpeereid
+    return ER_OK;
+#else
+    int enableCred = 1;
+    int rc = setsockopt(sockFd, SOL_SOCKET, SO_PASSCRED, &enableCred, sizeof(enableCred));
+    if (rc == -1) {
+        QCC_LogError(status, ("UnixTransport(): setsockopt(SO_PASSCRED) failed"));
+        qcc::Close(sockFd);
+        return ER_OS_ERROR;
+    }
+
+    /*
+     * Compose a header that includes the local user credentials and a single NUL byte.
+     */
+    ssize_t ret;
+    char nulbuf = 0;
+    struct cmsghdr* cmsg;
+    struct ucred* cred;
+    struct iovec iov[] = { { &nulbuf, sizeof(nulbuf) } };
+    char cbuf[CMSG_SPACE(sizeof(struct ucred))];
+    ::memset(cbuf, 0, sizeof(cbuf));
+    struct msghdr msg;
+    msg.msg_name = NULL;
+    msg.msg_namelen = 0;
+    msg.msg_iov = iov;
+    msg.msg_iovlen = ArraySize(iov);
+    msg.msg_control = cbuf;
+    msg.msg_controllen = ArraySize(cbuf);
+    msg.msg_flags = 0;
+
+    cmsg = CMSG_FIRSTHDR(&msg);
+    cmsg->cmsg_level = SOL_SOCKET;
+    cmsg->cmsg_type = SCM_CREDENTIALS;
+    cmsg->cmsg_len = CMSG_LEN(sizeof(struct ucred));
+    cred = reinterpret_cast<struct ucred*>(CMSG_DATA(cmsg));
+    cred->uid = uid;
+    cred->gid = gid;
+    cred->pid = pid;
+
+    QCC_DbgHLPrintf(("Sending UID: %u  GID: %u  PID %u", cred->uid, cred->gid, cred->pid));
+
+    ret = sendmsg(sockFd, &msg, 0);
+    if (ret != 1) {
+        return ERR_OS_ERROR;
+    }
+
+    /*
+     * If we don't disable this every read will have credentials which adds overhead if have
+     * enabled unix file descriptor passing.
+     */
+    enableCred = 0;
+    rc = setsockopt(sockFd, SOL_SOCKET, SO_PASSCRED, &enableCred, sizeof(enableCred));
+    if (rc == -1) {
+        QCC_LogError(status, ("UnixTransport(): setsockopt(SO_PASSCRED) failed"));
+    }
+
+    return ERR_OK;
+#endif
+}
+
 QStatus UnixTransport::Connect(const char* connectArgs, RemoteEndpoint** newep)
 {
     /*
@@ -290,47 +358,11 @@ QStatus UnixTransport::Connect(const char* connectArgs, RemoteEndpoint** newep)
 
     isConnected = true;
 
-    int enableCred = 1;
-    int rc = setsockopt(sockFd, SOL_SOCKET, SO_PASSCRED, &enableCred, sizeof(enableCred));
-    if (rc == -1) {
-        QCC_LogError(status, ("UnixTransport(): setsockopt(SO_PASSCRED) failed"));
-        qcc::Close(sockFd);
-        return ER_OS_ERROR;
-    }
-
-    /*
-     * Compose a header that includes the local user credentials and a single NUL byte.
-     */
-    ssize_t ret;
-    char nulbuf = 0;
-    struct cmsghdr* cmsg;
-    struct ucred* cred;
-    struct iovec iov[] = { { &nulbuf, sizeof(nulbuf) } };
-    char cbuf[CMSG_SPACE(sizeof(struct ucred))];
-    ::memset(cbuf, 0, sizeof(cbuf));
-    struct msghdr msg;
-    msg.msg_name = NULL;
-    msg.msg_namelen = 0;
-    msg.msg_iov = iov;
-    msg.msg_iovlen = ArraySize(iov);
-    msg.msg_control = cbuf;
-    msg.msg_controllen = ArraySize(cbuf);
-    msg.msg_flags = 0;
-
-    cmsg = CMSG_FIRSTHDR(&msg);
-    cmsg->cmsg_level = SOL_SOCKET;
-    cmsg->cmsg_type = SCM_CREDENTIALS;
-    cmsg->cmsg_len = CMSG_LEN(sizeof(struct ucred));
-    cred = reinterpret_cast<struct ucred*>(CMSG_DATA(cmsg));
-    cred->uid = GetUid();
-    cred->gid = GetGid();
-    cred->pid = GetPid();
-
-    QCC_DbgHLPrintf(("Sending UID: %u  GID: %u  PID %u", cred->uid, cred->gid, cred->pid));
-
     UnixEndpoint* conn = NULL;
-    ret = sendmsg(sockFd, &msg, 0);
-    if (ret == 1) {
+
+    status = SendSocketCreds(sockFd, GetUid(), GetGid(), GetPid());
+    if (status == ER_OK) {
+        conn = new UnixEndpoint(m_bus, false, normSpec, sockFd);
         m_endpointListLock.Lock();
         if (m_stopping) {
             m_endpointListLock.Unlock();
@@ -389,15 +421,6 @@ QStatus UnixTransport::Connect(const char* connectArgs, RemoteEndpoint** newep)
             *newep = NULL;
         }
     } else {
-        /*
-         * If we don't disable this every read will have credentials which adds overhead if have
-         * enabled unix file descriptor passing.
-         */
-        int disableCred = 0;
-        rc = setsockopt(sockFd, SOL_SOCKET, SO_PASSCRED, &disableCred, sizeof(disableCred));
-        if (rc == -1) {
-            QCC_LogError(status, ("UnixTransport(): setsockopt(SO_PASSCRED) failed"));
-        }
         if (newep) {
             *newep = conn;
         }

daemon/BTTransport.cc
@@ -38,7 +38,11 @@
 #include "BTTransport.h"
 
 #if defined QCC_OS_GROUP_POSIX
+#if defined(QCC_OS_DARWIN)
+#warning Darwin support for bluetooth to be implemented
+#else
 #include "bt_bluez/BTAccessor.h"
+#endif
 #elif defined QCC_OS_GROUP_WINDOWS
 #error Windows support to be implemented
 #endif

daemon/DaemonUnixTransport.cc
@@ -222,6 +222,81 @@ void DaemonUnixTransport::EndpointExit(RemoteEndpoint* ep)
     delete uep;
 }
 
+static QStatus GetSocketCreds(SocketFd sockFd, uid_t*uid, gid_t*gid, pid_t*pid)
+{
+    QStatus status = ER_OK;
+#if defined(QCC_OS_DARWIN)
+    *pid = 0;
+    int ret = getpeereid(sockFd, uid, gid);
+    if (ret == -1) {
+        status = ER_OS_ERROR;
+        qcc::Close(sockFd);
+    }
+#else
+    int enableCred = 1;
+    int ret = setsockopt(sockFd, SOL_SOCKET, SO_PASSCRED, &enableCred, sizeof(enableCred));
+    if (ret == -1) {
+        status = ER_OS_ERROR;
+        qcc::Close(sockFd);
+    }
+
+    if (status == ER_OK) {
+        qcc::String authName;
+        DaemonUnixEndpoint* conn;
+        ssize_t ret;
+        char nulbuf = 255;
+        struct cmsghdr* cmsg;
+        struct iovec iov[] = { { &nulbuf, sizeof(nulbuf) } };
+        struct msghdr msg;
+        char cbuf[CMSG_SPACE(sizeof(struct ucred))];
+        msg.msg_name = NULL;
+        msg.msg_namelen = 0;
+        msg.msg_iov = iov;
+        msg.msg_iovlen = ArraySize(iov);
+        msg.msg_flags = 0;
+        msg.msg_control = cbuf;
+        msg.msg_controllen = CMSG_LEN(sizeof(struct ucred));
+
+        while (true) {
+            ret = recvmsg(newSock, &msg, 0);
+            if (ret == -1) {
+                if (errno == EWOULDBLOCK) {
+                    qcc::Event event(newSock, qcc::Event::IO_READ, false);
+                    status = Event::Wait(event, CRED_TIMEOUT);
+                    if (status != ER_OK) {
+                        QCC_LogError(status, ("Credentials exhange timeout"));
+                        break;
+                    }
+                } else {
+                    break;
+                }
+            } else {
+                break;
+            }
+        }
+
+        if ((ret != 1) && (nulbuf != 0)) {
+            qcc::Close(sockFd);
+            status = ER_READ_ERROR;
+        }
+
+        if (status == ER_OK) {
+            for (cmsg = CMSG_FIRSTHDR(&msg); cmsg != NULL; cmsg = CMSG_NXTHDR(&msg, cmsg)) {
+                if ((cmsg->cmsg_level == SOL_SOCKET) && (cmsg->cmsg_type == SCM_CREDENTIALS)) {
+                    struct ucred* cred = reinterpret_cast<struct ucred*>(CMSG_DATA(cmsg));
+                    *uid = cred->uid;
+                    *gid = cred->gid;
+                    *pid = cred->pid;
+                    QCC_DbgHLPrintf(("Received UID: %u  GID: %u  PID %u", cred->uid, cred->gid, cred->pid));
+                }
+            }
+        }
+    }
+#endif
+
+    return status;
+}
+
 void* DaemonUnixTransport::Run(void* arg)
 {
     QStatus status = ER_OK;
@@ -278,66 +353,22 @@ void* DaemonUnixTransport::Run(void* arg)
 
             status = Accept((*i)->GetFD(), newSock);
 
+            uid_t uid;
+            gid_t gid;
+            pid_t pid;
+
             if (status == ER_OK) {
-                int enableCred = 1;
-                int ret = setsockopt(newSock, SOL_SOCKET, SO_PASSCRED, &enableCred, sizeof(enableCred));
-                if (ret == -1) {
-                    status = ER_OS_ERROR;
-                    qcc::Close(newSock);
-                }
+                status = GetSocketCreds(newSock, &uid, &gid, &pid);
             }
 
             if (status == ER_OK) {
                 qcc::String authName;
                 DaemonUnixEndpoint* conn;
-                ssize_t ret;
-                char nulbuf = 255;
-                struct cmsghdr* cmsg;
-                struct iovec iov[] = { { &nulbuf, sizeof(nulbuf) } };
-                struct msghdr msg;
-                char cbuf[CMSG_SPACE(sizeof(struct ucred))];
-                msg.msg_name = NULL;
-                msg.msg_namelen = 0;
-                msg.msg_iov = iov;
-                msg.msg_iovlen = ArraySize(iov);
-                msg.msg_flags = 0;
-                msg.msg_control = cbuf;
-                msg.msg_controllen = CMSG_LEN(sizeof(struct ucred));
-
-                while (true) {
-                    ret = recvmsg(newSock, &msg, 0);
-                    if (ret == -1) {
-                        if (errno == EWOULDBLOCK) {
-                            qcc::Event event(newSock, qcc::Event::IO_READ, false);
-                            status = Event::Wait(event, CRED_TIMEOUT);
-                            if (status != ER_OK) {
-                                QCC_LogError(status, ("Credentials exhange timeout"));
-                                break;
-                            }
-                        } else {
-                            break;
-                        }
-                    } else {
-                        break;
-                    }
-                }
-
-                if ((ret != 1) && (nulbuf != 0)) {
-                    qcc::Close(newSock);
-                    continue;
-                }
 
                 conn = new DaemonUnixEndpoint(m_bus, true, "", newSock);
-
-                for (cmsg = CMSG_FIRSTHDR(&msg); cmsg != NULL; cmsg = CMSG_NXTHDR(&msg, cmsg)) {
-                    if ((cmsg->cmsg_level == SOL_SOCKET) && (cmsg->cmsg_type == SCM_CREDENTIALS)) {
-                        struct ucred* cred = reinterpret_cast<struct ucred*>(CMSG_DATA(cmsg));
-                        conn->SetUserId(cred->uid);
-                        conn->SetGroupId(cred->gid);
-                        conn->SetProcessId(cred->pid);
-                        QCC_DbgHLPrintf(("Received UID: %u  GID: %u  PID %u", cred->uid, cred->gid, cred->pid));
-                    }
-                }
+                conn->SetUserId(uid);
+                conn->SetGroupId(gid);
+                conn->SetProcessId(pid);
 
                 /* Initialized the features for this endpoint */
                 conn->GetFeatures().isBusToBus = false;
@@ -363,7 +394,7 @@ void* DaemonUnixTransport::Run(void* arg)
                     delete conn;
                     conn = NULL;
                 }
-            } else if (ER_WOULDBLOCK == status) {
+            } else if (ER_WOULDBLOCK == status || ER_READ_ERROR == status) {
                 status = ER_OK;
             }
 

daemon/NameService.cc
@@ -32,7 +32,7 @@
 #include <ws2tcpip.h>
 #include <iphlpapi.h>
 
-#else // defined (QCC_OS_ANDROID) or defined (QCC_OS_LINUX)
+#elif defined(QCC_OS_GROUP_POSIX)
 
 #include <errno.h>
 #include <netdb.h>
@@ -41,10 +41,19 @@
 #include <arpa/inet.h>
 #include <sys/socket.h>
 #include <sys/ioctl.h>
+
+#if defined(QCC_OS_ANDROID) || defined(QCC_OS_LINUX)
 #include <linux/netlink.h>
 #include <linux/rtnetlink.h>
+#endif
 
-#endif // defined(QCC_OS_WINDOWS)
+#if defined(QCC_OS_DARWIN)
+#include <ifaddrs.h>
+#define IPV6_DROP_MEMBERSHIP IPV6_LEAVE_GROUP
+#define IPV6_ADD_MEMBERSHIP IPV6_JOIN_GROUP
+#endif
+
+#endif // defined(QCC_OS_GROUP_POSIX)
 
 #include <qcc/platform.h>
 #include <qcc/Debug.h>
@@ -260,6 +269,42 @@ NameService::NameService()
 // become available, we organize the output that way instead of the more OS-like
 // way of an interface with an associated list of addresses.
 //
+
+#if defined (QCC_OS_GROUP_POSIX)
+
+static uint32_t TranslateFlags(uint32_t flags)
+{
+    uint32_t ourFlags = 0;
+    if (flags & IFF_UP) ourFlags |= NameService::IfConfigEntry::UP;
+    if (flags & IFF_BROADCAST) ourFlags |= NameService::IfConfigEntry::BROADCAST;
+    if (flags & IFF_DEBUG) ourFlags |= NameService::IfConfigEntry::DEBUG;
+    if (flags & IFF_LOOPBACK) ourFlags |= NameService::IfConfigEntry::LOOPBACK;
+    if (flags & IFF_POINTOPOINT) ourFlags |= NameService::IfConfigEntry::POINTOPOINT;
+    if (flags & IFF_RUNNING) ourFlags |= NameService::IfConfigEntry::RUNNING;
+    if (flags & IFF_NOARP) ourFlags |= NameService::IfConfigEntry::NOARP;
+    if (flags & IFF_PROMISC) ourFlags |= NameService::IfConfigEntry::PROMISC;
+    if (flags & IFF_NOTRAILERS) ourFlags |= NameService::IfConfigEntry::NOTRAILERS;
+    if (flags & IFF_ALLMULTI) ourFlags |= NameService::IfConfigEntry::ALLMULTI;
+#if defined(IFF_MASTER)
+    if (flags & IFF_MASTER) ourFlags |= NameService::IfConfigEntry::MASTER;
+#endif
+#if defined(IFF_SLAVE)
+    if (flags & IFF_SLAVE) ourFlags |= NameService::IfConfigEntry::SLAVE;
+#endif
+    if (flags & IFF_MULTICAST) ourFlags |= NameService::IfConfigEntry::MULTICAST;
+#if defined(IFF_PORTSEL)
+    if (flags & IFF_PORTSEL) ourFlags |= NameService::IfConfigEntry::PORTSEL;
+#endif
+#if defined(IFF_AUTOMEDIA)
+    if (flags & IFF_AUTOMEDIA) ourFlags |= NameService::IfConfigEntry::AUTOMEDIA;
+#endif
+#if defined(IFF_DYNAMIC)
+    if (flags & IFF_DYNAMIC) ourFlags |= NameService::IfConfigEntry::DYNAMIC;
+#endif
+    return ourFlags;
+}
+
+#endif
 
 #if defined(QCC_OS_LINUX) || defined(QCC_OS_ANDROID)
 
@@ -536,29 +581,6 @@ static std::list<AddrEntry> NetlinkGetAddresses(uint32_t family)
     return entries;
 }
 
-static uint32_t TranslateFlags(uint32_t flags)
-{
-    uint32_t ourFlags = 0;
-    if (flags & IFF_UP) ourFlags |= NameService::IfConfigEntry::UP;
-    if (flags & IFF_BROADCAST) ourFlags |= NameService::IfConfigEntry::BROADCAST;
-    if (flags & IFF_DEBUG) ourFlags |= NameService::IfConfigEntry::DEBUG;
-    if (flags & IFF_LOOPBACK) ourFlags |= NameService::IfConfigEntry::LOOPBACK;
-    if (flags & IFF_POINTOPOINT) ourFlags |= NameService::IfConfigEntry::POINTOPOINT;
-    if (flags & IFF_RUNNING) ourFlags |= NameService::IfConfigEntry::RUNNING;
-    if (flags & IFF_NOARP) ourFlags |= NameService::IfConfigEntry::NOARP;
-    if (flags & IFF_PROMISC) ourFlags |= NameService::IfConfigEntry::PROMISC;
-    if (flags & IFF_NOTRAILERS) ourFlags |= NameService::IfConfigEntry::NOTRAILERS;
-    if (flags & IFF_ALLMULTI) ourFlags |= NameService::IfConfigEntry::ALLMULTI;
-    if (flags & IFF_MASTER) ourFlags |= NameService::IfConfigEntry::MASTER;
-    if (flags & IFF_SLAVE) ourFlags |= NameService::IfConfigEntry::SLAVE;
-    if (flags & IFF_MULTICAST) ourFlags |= NameService::IfConfigEntry::MULTICAST;
-    if (flags & IFF_PORTSEL) ourFlags |= NameService::IfConfigEntry::PORTSEL;
-    if (flags & IFF_AUTOMEDIA) ourFlags |= NameService::IfConfigEntry::AUTOMEDIA;
-    if (flags & IFF_DYNAMIC) ourFlags |= NameService::IfConfigEntry::DYNAMIC;
-    return ourFlags;
-}
-
-
 QStatus NameService::IfConfig(std::vector<IfConfigEntry>& entries)
 {
     QCC_DbgPrintf(("NameService::IfConfig(): The Linux way\n"));
@@ -729,6 +751,73 @@ QStatus NameService::IfConfig(std::vector<IfConfigEntry>& entries)
     delete [] parray;
 
     return ER_OK;
+}
+
+#elif defined(QCC_OS_DARWIN)
+
+QStatus NameService::IfConfig(std::vector<IfConfigEntry>& entries)
+{
+    QCC_DbgPrintf(("NameService::IfConfig(): The Darwin way\n"));
+
+    int sck;
+    struct ifaddrs* iflist = NULL;
+    QStatus status = ER_OK;
+
+    sck = socket(AF_INET, SOCK_DGRAM, 0);
+    if (sck < 0) {
+        status = ER_OS_ERROR;
+        QCC_LogError(status, ("Opening socket: %s", strerror(errno)));
+        goto exit;
+    }
+
+    if (getifaddrs(&iflist) < 0) {
+        status = ER_OS_ERROR;
+        goto exit;
+    }
+
+    for (struct ifaddrs* if_addr = iflist; if_addr != NULL; if_addr = if_addr->ifa_next) {
+        IfConfigEntry entry;
+        struct ifreq if_item;
+        char buf[INET6_ADDRSTRLEN];
+
+        *buf = 0;
+        if (if_addr->ifa_addr->sa_family == AF_INET) {
+            struct in_addr*p =  &((struct sockaddr_in*)if_addr->ifa_addr)->sin_addr;
+            inet_ntop(AF_INET, p, buf, sizeof(buf));
+        } else if (if_addr->ifa_addr->sa_family == AF_INET6) {
+            struct in6_addr*p =  &((struct sockaddr_in6*)if_addr->ifa_addr)->sin6_addr;
+            inet_ntop(AF_INET6, p, buf, sizeof(buf));
+            entry.m_addr = qcc::String(buf);
+        }
+
+        strncpy(if_item.ifr_name, if_addr->ifa_name, IFNAMSIZ);
+        if (ioctl(sck, SIOCGIFMTU, &if_item) < 0) {
+            status = ER_OS_ERROR;
+            QCC_LogError(status, ("Calling IOCtl: %s", strerror(errno)));
+            goto exit;
+        }
+
+        entry.m_name = qcc::String(if_addr->ifa_name);
+        entry.m_family = if_addr->ifa_addr->sa_family;
+        entry.m_flags = TranslateFlags(if_addr->ifa_flags);
+        entry.m_index = if_nametoindex(if_addr->ifa_name);
+        entry.m_mtu = if_item.ifr_mtu;
+        if (*buf != 0) {
+            entry.m_addr = qcc::String(buf);
+        }
+
+        entries.push_back(entry);
+    }
+exit:
+    if (sck >= 0) {
+        close(sck);
+    }
+    if (iflist) {
+        freeifaddrs(iflist);
+    }
+    return status;
+
+
 }
 
 #else
@@ -1525,6 +1614,7 @@ void NameService::LazyUpdateInterfaces(void)
             struct ip_mreq mreq;
             mreq.imr_multiaddr.s_addr = inet_addr(IPV4_MULTICAST_GROUP);
             mreq.imr_interface.s_addr = address.GetIPv4AddressNetOrder();
+
             if (setsockopt(sockFd, IPPROTO_IP, IP_ADD_MEMBERSHIP, reinterpret_cast<const char*>(&mreq), sizeof(mreq)) < 0) {
                 QCC_LogError(status, (
                                  "NameService::LazyUpdateInterfaces(): setsockopt(IP_ADD_MEMBERSHIP) failed: %d - %s",
@@ -1553,6 +1643,7 @@ void NameService::LazyUpdateInterfaces(void)
                 qcc::Close(sockFd);
                 continue;
             }
+
         }
 
         //

daemon/SConscript
@@ -14,11 +14,16 @@
 # 
 
 Import('env')
+from os.path import basename
 
-srcs = [ f for f in env.Glob('*.cc') + env.Glob('*.c')
-         if not (env['OS_GROUP'] == 'windows' and (str(f).endswith('DaemonUnixTransport.cc') or
-                                                   str(f).endswith('BTController.cc') or
-                                                   str(f).endswith('BTTransport.cc'))) ]
+srcs = [ f for f in env.Glob('*.cc') + env.Glob('*.c') ]
+bt_srcs = [ 'BTController.cc', 'BTTransport.cc', 'BTNodeDB.cc' ]
+
+if env['OS'] == "windows":
+    srcs = [ f for f in srcs if basename(str(f)) not in ['DaemonUnixTransport.cc'] + bt_srcs ]
+elif env['OS'] == "darwin":
+    srcs = [ f for f in srcs if basename(str(f)) not in bt_srcs ]
+
 daemon_objs = env.Object(srcs)
 config_objs = env.Object(['ConfigDB.cc',
                           'ServiceDB.cc',
@@ -26,7 +31,7 @@ config_objs = env.Object(['ConfigDB.cc',
                           'PolicyDB.cc'])
 
 # Select BlueZ or BM3 for bluetooth support
-if env['OS'] == 'windows' or env['OS'] == 'android_donut':
+if env['OS'] == 'windows' or env['OS'] == 'android_donut' or env['OS'] =="darwin":
     # Skip bluetooth
     #env.Append(CPPPATH=['bt_bm3'])
     #bm3_srcs, bm3_objs = env.SConscript('bt_bm3/SConscript')

daemon/posix/daemon-main.cc
@@ -50,17 +50,27 @@
 #include "Transport.h"
 #include "DaemonTCPTransport.h"
 #include "DaemonUnixTransport.h"
+
+#if defined(QCC_OS_DARWIN)
+#warning BT Support on Darwin needs to be implemented
+#else
 #include "BTTransport.h"
+#endif
+
 #include "Bus.h"
 #include "BusController.h"
 #include "ConfigDB.h"
 
 #if !defined(DAEMON_LIB)
+
+#if defined(QCC_OS_LINUX)
 #include <sys/prctl.h>
 #include <linux/capability.h>
 extern "C" {
 extern int capset(cap_user_header_t hdrp, const cap_user_data_t datap);
 }
+#endif
+
 #if defined(QCC_OS_ANDROID)
 #define BLUETOOTH_UID 1002
 #endif
@@ -465,7 +475,11 @@ int daemon(OptParse& opts)
     TransportFactoryContainer cntr;
     cntr.Add(new TransportFactory<DaemonTCPTransport>("tcp", false));
     cntr.Add(new TransportFactory<DaemonUnixTransport>("unix", false));
+#if defined(QCC_OS_DARWIN)
+#warning BT transport factory needs to be implemented for Darwin
+#else
     cntr.Add(new TransportFactory<BTTransport>("bluetooth", false));
+#endif
 
     Bus ajBus("alljoyn-daemon", cntr, listenSpecs.c_str());
     BusController ajBusController(ajBus, status);
@@ -638,8 +652,10 @@ int main(int argc, char** argv, char** env)
 
 #if !defined(DAEMON_LIB)
     if (!opts.GetNoSwitchUser()) {
+#if defined(QCC_OS_LINUX)
         // Keep all capabilities before switching users
         prctl(PR_SET_KEEPCAPS, 1, 0, 0, 0);
+#endif
 
 #if defined(QCC_OS_ANDROID)
         // Android uses hard coded UIDs.
@@ -667,6 +683,7 @@ int main(int argc, char** argv, char** env)
         }
 #endif
 
+#if defined(QCC_OS_LINUX)
         // Set the capabilities we need.
         struct __user_cap_header_struct header;
         struct __user_cap_data_struct cap;
@@ -678,6 +695,7 @@ int main(int argc, char** argv, char** env)
         cap.effective = cap.permitted;
         cap.inheritable = 0;
         capset(&header, &cap);
+#endif
     }
 #endif
 

daemon/test/bbdaemon.cc
@@ -39,7 +39,11 @@
 #include "DaemonTCPTransport.h"
 #if !defined(QCC_OS_WINDOWS)
 #include "DaemonUnixTransport.h"
+#if defined(QCC_OS_DARWIN)
+#warning BT needs to be implemented on Darwin
+#else
 #include "BTTransport.h"
+#endif
 #endif
 #include "Bus.h"
 #include "BusController.h"
@@ -445,7 +449,9 @@ int main(int argc, char** argv)
 
 #if !defined(QCC_OS_WINDOWS)
     cntr.Add(new TransportFactory<DaemonUnixTransport>("unix", false));
+#if !defined(QCC_OS_DARWIN)
     cntr.Add(new TransportFactory<BTTransport>("bluetooth", false));
+#endif
 #endif
 
     /* Create message bus with support for alternate transports */

daemon/test/hci.cc
@@ -84,6 +84,8 @@ int main(int argc, char** argv)
 {
 #ifdef WIN32
     printf("Not implemented on windows\n");
+#elif defined(QCC_OS_DARWIN)
+    printf("Not implemented on darwin\n");
 #else
     QStatus status;
     SocketFd hciFd;

commit 9c5279389be66d014c7b1107b75dfac0ae2f6890

Fix typo introduced in commit 63c988325161a3d9d37d that caused an extra UnixEndpoint to be allocated.
This showed itself as running out of file descriptors under Android.

src/UnixTransport.cc
@@ -362,7 +362,6 @@ QStatus UnixTransport::Connect(const char* connectArgs, const SessionOpts& opts,
 
     status = SendSocketCreds(sockFd, GetUid(), GetGid(), GetPid());
     if (status == ER_OK) {
-        conn = new UnixEndpoint(m_bus, false, normSpec, sockFd);
         m_endpointListLock.Lock();
         if (m_stopping) {
             m_endpointListLock.Unlock();